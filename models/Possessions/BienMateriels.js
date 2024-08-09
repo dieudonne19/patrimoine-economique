de8504cdebaf234601509a4b4d8da8f5a2e15126
Dieudonne <hei.dieudonne.19@gmail.com>
2024-08-09 15:33:47 +0300

    fix: nothing

--- models/Possessions/BienMateriels.js
+++ models/Possessions/BienMateriels.js
@@ -1,4 +1,5 @@
 import {Possession} from "./Possession.js";
+import { Personne } from "../Personne.js";
 
 export class BienMateriels extends Possession {
 
@@ -18,8 +19,8 @@ export class BienMateriels extends Possession {
      * Juste application du taux d'amortissement sur la valeur du bien materiel
      * @param {Date} dateDonnee
      */
-    getAmortissementAt (dateDonnee) {
-        if (super.getDateDebut <= dateDonnee) {
+    applyAmortissement (dateDonnee) {
+        if (super.getDateDebut <= new Date(dateDonnee)) {
             const intervalDeMois = new Date(dateDonnee).getMonth() - super.getDateDebut.getMonth();
             const intervalDeAnnee =  new Date(dateDonnee).getFullYear() - super.getDateDebut.getFullYear();
             const intervalDeJours = new Date(dateDonnee).getDay() - super.getDateDebut.getDay();
@@ -27,17 +28,17 @@ export class BienMateriels extends Possession {
 
             // console.log(`annee ${intervalDeAnnee} / mois ${intervalDeMois} / jours ${intervalDeJours}`)
 
-            const valeurAmortissement = 
-            (this.valeur * (this.amortissement / 100)) * (nombreMois / 12 + intervalDeJours /365);
-            this.valeur -= valeurAmortissement;
-        };
+            const valeurAmortissement = (this.valeur * (this.amortissement / 100)) * (nombreMois / 12 + intervalDeJours / 365);
+            return Math.round(valeurAmortissement);
+        } else return 0;
     }
 
     getValeurAt(dateDonnee) {
         if (new Date(dateDonnee) < super.getDateDebut) return 0;
         else {
-            this.getAmortissementAt(dateDonnee);
-            return Math.round(this.valeur);
+            const amortis = this.applyAmortissement(dateDonnee);
+            // console.log(amortis);
+            return Math.round(this.valeur - amortis);
         }
     }
 
@@ -45,3 +46,9 @@ export class BienMateriels extends Possession {
         return this.type;
     }
 }
+
+
+// const Ilo = new Personne("Ilo");
+
+// const portable = new BienMateriels(Ilo, "ordinateur portable", 300000, "materiel informatique", "2024-08-08");
+// console.log(portable.getValeurAt("2025-08-08"));
